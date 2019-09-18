561735611b4adaf42e4f2338ba39d907b0ddf75a
Noiredd <snowball91b@gmail.com>
2019-09-18 15:18:56 +0200

    repository path validation

--- config.py
+++ config.py
@@ -5,7 +5,34 @@ import shutil
 
 CONFIG_FILE = 'config.json'
 
+def validate_data_path(data_path):
+  """Make sure the requested data path will be okay for use.
+
+  Return False when:
+    path does not exist AND cannot be created with mkdir
+    path cannot be written to
+  otherwise, creates the folder and returns True.
+  Additionally, will issue a textual warning if the folder is not empty.
+  """
+  if os.path.isdir(data_path):
+    try:
+      open(os.path.join(data_path, 'test'), 'w').close()
+    except:
+      print("Error: this path is not writable!")
+      return False
+    os.remove(os.path.join(data_path, 'test'))
+  else:
+    try:
+      os.mkdir(data_path)
+    except:
+      print("Error: this path does not exist and cannot be created!")
+      return False
+  if os.listdir(data_path):
+    print("Warning: the requested folder is not empty!")
+  return True
+
 def load_config(config_path):
+  """Loads existing config file."""
   with open(config_path, 'r') as config_file:
     config = json.loads(config_file.read())
   return config
@@ -16,6 +43,8 @@ def new_config(config_path):
   print("Your current path is: {}".format(os.getcwd()))
   data_path = input()
   data_path = os.path.abspath(data_path)
+  if not validate_data_path(data_path):
+    exit(-1)
   print("This path evaluates to: {}".format(data_path))
   config = {'data_path': data_path}
   with open(config_path, 'w') as config_file:
@@ -23,6 +52,7 @@ def new_config(config_path):
   return config
 
 def get_config():
+  """Tries to load config, creates a new one upon failure."""
   this_path = os.path.abspath(inspect.getfile(inspect.currentframe()))
   this_dir, _ = os.path.split(this_path)
   config_path = os.path.join(this_dir, CONFIG_FILE)
